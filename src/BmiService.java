ad49791fd22d1bed708aec24e6a162d0f1b134a8
Nadezhda <7_nadezhda@mail.ru>
2023-04-03 13:32:17 +0400

    update repo

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,8 @@ public class BmiService {
     public double calculate(double weight, double height) {
 
         double index = weight / Math.pow(height, 2);
-        return index;
+        int bmiIndex = (int) Math.round(index);
+        return bmiIndex;
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        double indexNorm = service.calculate(50, 1.5);
+        double indexNorm = service.calculate(98, 1.87);
         System.out.println(indexNorm);
 
         double indexMax = service.calculate(150, 1.5);
